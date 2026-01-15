6fa1f2fae0e7de29c18ea37e950de74100272395
Mimsseey <56138496+Mimsseey@users.noreply.github.com>
2026-01-16 00:17:04 +0100

    Update definitions.h

--- definitions.h
+++ definitions.h
@@ -1,34 +1,38 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
+// WiFi Access Point Configuration
 #define AP_SSID "ESP32-Deauther"
-#define AP_PASS "esp32wroom32"
-#define LED 2
-#define SERIAL_DEBUG
-#define CHANNEL_MAX 13
-#define NUM_FRAMES_PER_DEAUTH 16
-#define DEAUTH_BLINK_TIMES 2
-#define DEAUTH_BLINK_DURATION 20
-#define DEAUTH_TYPE_SINGLE 0
-#define DEAUTH_TYPE_ALL 1
+#define AP_PASS "lozinka32"  // Changed from "esp32wroom32" - you can customize this
 
-#ifdef SERIAL_DEBUG
+// LED Configuration
+#define LED 22
+
+// Network Configuration
+#define CHANNEL_MAX 11  // Channels 1-11 (12-13 not supported by all devices)
+
+// Deauth Attack Configuration
+#define NUM_FRAMES_PER_DEAUTH 32        // Number of deauth frames per station
+#define DEAUTH_BLINK_TIMES 3            // How many times LED blinks per attack
+#define DEAUTH_BLINK_DURATION 85       // Total blink duration in ms (50ms on, 50ms off per blink)
+
+// Attack Types
+#define DEAUTH_TYPE_SINGLE 0            // Target single network
+#define DEAUTH_TYPE_ALL 1               // Target all networks
+
+// Debug macros (always active now)
 #define DEBUG_PRINT(...) Serial.print(__VA_ARGS__)
 #define DEBUG_PRINTLN(...) Serial.println(__VA_ARGS__)
 #define DEBUG_PRINTF(...) Serial.printf(__VA_ARGS__)
-#endif
-#ifndef SERIAL_DEBUG
-#define DEBUG_PRINT(...)
-#define DEBUG_PRINTLN(...)
-#define DEBUG_PRINTF(...)
-#endif
+
+// LED blink macro
 #ifdef LED
 #define BLINK_LED(num_times, blink_duration) blink_led(num_times, blink_duration)
-#endif
-#ifndef LED
-#define BLINK_LED()
+#else
+#define BLINK_LED(num_times, blink_duration)
 #endif
 
+// Function declarations
 void blink_led(int num_times, int blink_duration);
 
-#endif
+#endif // DEFINITIONS_H
